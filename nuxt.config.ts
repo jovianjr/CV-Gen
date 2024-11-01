4f90f909f82326c1df5135d5bed3cfb94469b541
Jovian <alphonsusjovian@mail.ugm.ac.id>
2024-11-01 15:45:29 +0700

    add docs folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 import Aura from "@primevue/themes/aura";
 
 export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
   css: ["~/assets/css/main.css", "primeicons/primeicons.css"],
   devtools: { enabled: true },
   postcss: {
