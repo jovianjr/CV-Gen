7548bff397649451ddd4485f3763c48125e9755d
Jovian <alphonsusjovian@mail.ugm.ac.id>
2024-11-01 15:51:26 +0700

    feat(meta): update meta tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,17 @@
 import Aura from "@primevue/themes/aura";
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "CVGen",
+      meta: [
+        { name: "title", content: "CVGen" },
+        { name: "description", content: "Generate your cv ats friendly" },
+      ],
+    },
+  },
   compatibilityDate: "2024-04-03",
   css: ["~/assets/css/main.css", "primeicons/primeicons.css"],
   devtools: { enabled: true },
